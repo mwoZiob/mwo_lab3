0ad67e4310f80e584a7659070f96bcd00c6e48fb
Michał Ziober <michalziober08@gmail.com>
2022-11-17 11:32:56 +0100

    added version on pages

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,15 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
 
 namespace mwo4.Pages
 {
     public class IndexModel : PageModel
     {
+        public string ver;
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            string path = "../version.txt";
+            if(File.Exists(path))
+            {
+                ver = File.ReadAllText(path);
+            }
+            else
+            {
+                ver = "unknown";
+            }
         }
 
         public void OnGet()
