a387b75e04e529a204b715d136f950dfdcc71967
Michał Ziober <michalziober08@gmail.com>
2022-11-17 12:21:10 +0100

    changed version on app

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -13,7 +13,7 @@ namespace mwo4.Pages
         {
             _logger = logger;
             //string path = "../../../version.txt";
-            string path = "/home/runner/.dotnet/api/version.txt";
+            string path = "/home/site/wwwroot/version.txt";
             if(System.IO.File.Exists(path))
             {
                 ver = System.IO.File.ReadAllText(path);
